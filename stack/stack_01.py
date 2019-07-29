df843683cc0791f208395af30da976a1a9f2e9ed
JeevanGang <g1gangarde@gmail.com>
2019-07-29 15:29:13 +0530

    Stack DS V_01

---
+++ stack/stack_01.py
@@ -0,0 +1,29 @@
+
+class Stack():
+    '''Implementation of stack Data Structure'''
+
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        return self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+s = Stack()
+s.push(1)
+s.push(2)
+print(s.pop())
+print(s.peek())
+print(s.size())
